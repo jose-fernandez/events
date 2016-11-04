a2173cd362988ba275f13a12e552531ce55a764c
jose-fernandez <jose.fernandez91@gmail.com>
2016-11-04 10:05:58 +0100

    iniciando

--- ej10.js
+++ ej10.js
@@ -2,3 +2,29 @@
 10.Mejora el ejercicio anterior para que se pueda hacer en una página con un número
 cualquiera de imágenes.
 */
+
+function moveImage(){
+	var push=0;
+	var img=document.getElementById("image");
+	img.addEventListener("mousedown", function(e){
+		push = 1;
+		x=e.offsetX;
+		y=e.offsetY;
+	});
+	window.addEventListener("mousemove", function(a){
+		if(push<2){
+			a.preventDefault();
+			img.style.top=`${a.clientY-y}px`;
+			img.style.left=`${a.clientX-x}px`;		
+		}		
+	});
+	img.addEventListener("mouseup", function(i){
+		push=2;
+		x=e.offsetX;
+		y=e.offsetY;
+	});
+}
+
+window.onload=function(){
+	moveImage();
+}
